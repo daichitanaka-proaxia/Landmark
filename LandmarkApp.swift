44388f12d40861a846dfb68fc441481efb4f471d
Daichi Tanaka <daichi.tanaka@proaxia-consulting.co.jp>
2021-01-11 14:05:14 +0900

    Apple SwiftUI Tutorials Creating and Combining Views

---
+++ LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by H5266 on 2021/01/11.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
