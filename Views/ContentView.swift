0f8c618ad6d70b12438b67f421be87995f98ecdc
Daichi Tanaka <daichi.tanaka@proaxia-consulting.co.jp>
2021-01-11 14:58:25 +0900

    Apple SwiftUI Tutorials Building Lists and Navigation

---
+++ Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmark
+//
+//  Created by H5266 on 2021/01/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+         LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Views/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  Landmark
+//
+//  Created by H5266 on 2021/01/11.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks){ landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+                    LandmarkList()
+                        .previewDevice(PreviewDevice(rawValue: deviceName))
+                        .previewDisplayName(deviceName)
+                }
+    }
+}
